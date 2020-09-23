f9c2c1539f4d8ee0ac4992d89c7b1a6b509e011b
atiederire7 <atiederire1920@gmail.com>
2020-09-23 23:40:47 +0300

    update-23.09.2020.23:40

--- burger-css/js/script.js
+++ burger-css/js/script.js
@@ -1,9 +1,6 @@
-$(document).ready(function(){
-    $('.block__title').click(function(event) {
-        if($('.block').hasClass('one')){
-            $('.block__title').not($(this)).removeClass('active');
-            $('.block__text').not($(this).next()).slideUp(300);
-        }
-        $(this).toggleClass('active').next().slideToggle(300);
-    });
+$(document).ready(function() {
+	$('.header__burger').click(function(event) {
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
 });
